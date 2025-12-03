c5ee5832a376d08eb4b41bc469dc2ccaedbbc39c
jjnnugent <jjnnugent@gmail.com>
2025-12-03 01:14:19 -0800

    feat: resurrected tftbot

---
+++ tft_json.py
@@ -0,0 +1,65 @@
+import logging
+from logging.config import fileConfig
+import json
+import os
+import re
+
+from curl_cffi import requests
+from utils.file import save_json
+from utils.slack import SlackHandler
+
+
+def dict_keys(dump: dict, end: str) -> list:
+    keys = ["props", "pageProps", "dehydratedState", "queries", "state", "data", end]
+    step = dump
+    for key in keys:
+        step = step.get(key, {})
+        if not step:
+            return []
+
+        if key == "queries":
+            if end == "champions":
+                step = step[0]
+            else:
+                step = step[2]
+    return step
+
+
+if __name__ == "__main__":
+    fileConfig(fname=os.path.expanduser("~/logs/logging.conf"))
+    logger = logging.getLogger("gettft")
+    logger.addHandler(hdlr=SlackHandler())
+
+    response = requests.get(url=os.getenv("URL_TF"), impersonate="chrome")
+    if response.status_code == 200:
+        re_json = re.search(
+            pattern=r"\"__NEXT_DATA__\".*?\>([^<]+)\<", string=response.text, flags=re.I
+        )
+
+        if re_json:
+            json_data = json.loads(re_json.group(1))
+
+            champs = dict_keys(dump=json_data, end="champions")
+            items = dict_keys(dump=json_data, end="items")
+            new = dict()
+
+            for result in champs:
+                bis = result.get("recommendItems")
+                if bis:
+                    name = result.get("key").lower()
+                    new[name] = bis
+
+                    for i in range(len(bis)):
+                        for item in items:
+                            item_name = item.get("ingameKey")
+                            if item_name and item_name == bis[i]:
+                                bis[i] = "http:" + item.get("imageUrl")
+                else:
+                    continue
+
+            save_json(data=new, name=os.path.expanduser("~/data/tft.json"))
+
+        else:
+            logger.warning("data not found")
+    else:
+        logger.warning("%s returned", response.status_code)
---
+++ tftbot.py
@@ -0,0 +1,88 @@
+import logging
+import os
+import re
+
+from dotenv import load_dotenv
+from slack_bolt import App
+
+from utils.file import load_json
+
+logging.basicConfig(level=logging.INFO)
+load_dotenv()
+
+tftbot = App(
+    signing_secret=os.getenv("TFTBOT_SS"),
+    token=os.getenv("TFTBOT_TOKEN"),
+)
+
+
+@tftbot.event("message")
+def message_event(event, logger, say):
+    logger.info(event)
+    name = event.get("text")
+    if name == "sol":
+        name = "aurelionsol"
+    if name == "baron" or name == "nashor":
+        name = "baronnashor"
+    if name == "blitz":
+        name = "blitzcrank"
+    if name == "cho":
+        name = "chogath"
+    if name.startswith("dr"):
+        name = "drmundo"
+    if name == "gp":
+        name = "gangplank"
+    if name.startswith("il"):
+        name = "illaoi"
+    if name == "jarvan":
+        name = "jarvaniv"
+    if name == "kobuko" or name == "yuumi":
+        name = "kobukoyuumi"
+    if name.startswith("malz"):
+        name = "malzahar"
+    if name == "mf":
+        name = "missfortune"
+    if name == "lucian" or name == "senna":
+        name = "luciansenna"
+    if name == "nut":
+        name = "nautilus"
+    if name.startswith("qu") or name == "yana":
+        name = "quiyana"
+    if name == "rift" or name == "harold":
+        name = "riftherald"
+    if name.startswith("sej"):
+        name = "sejuani"
+    if name.startswith("tom") or name.startswith("tahm"):
+        name = "tahmkench"
+    if name.startswith("try"):
+        name = "tryndamere"
+    if name == "tf":
+        name = "twistedfate"
+    if name.startswith("xin"):
+        name = "xinzhao"
+
+    data = load_json(os.path.expanduser("~/data/tft.json"))
+    if name in data.keys():
+        images = data.get(name)
+        elems = []
+        for src in images:
+            elems.append(
+                {
+                    "type": "image",
+                    "image_url": src,
+                    "alt_text": "",
+                }
+            )
+
+        block = [
+            {
+                "type": "context",
+                "elements": elems,
+            }
+        ]
+
+        say(text=f"{name} bis", blocks=block)
+
+
+if __name__ == "__main__":
+    tftbot.start(port=52259)
